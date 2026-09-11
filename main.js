c8a631ac7881ae66699d1efbf6a0c2f91553c7e8
Harrietta18 <erhurheharrietta@gmail.com>
2026-09-11 15:22:43 +0100

    added if stataement

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let age =Number(prompt("Enter your age"))
+
+if(age < 18){
+    console.log("You are a minor")
+} else if(age >= 18 && age < 65){
+    console.log("You are an adult")
+}
+
+
 let name = "Harriet"
 console.log(name)
 
